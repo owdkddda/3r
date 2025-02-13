fd31daf6cf741cafe1b3a49caf4337bc269c4643
owdkddda <1111039@siswa.karangturi.sch.id>
2025-02-13 16:47:01 +0700

    PERUBAHAN 13 fEBRUARI 2025
    WDA

---
+++ Assets/Scenes/Script/CompanionFollow/enemy follow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class enemyfollow : MonoBehaviour
+{
+
+    public Transform player;
+    public float followSpeed;
+    public float followDistance;
+
+        void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance > followDistance)
+        {
+            Vector3 targetPosition = player.position;
+            targetPosition.z = transform.position.z;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scenes/Script/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  
+    public float speed = 5f;
+
+    public Camera mainCamera;
+    // Update is called once per frame
+
+    void Update()
+    {
+        float moveX = Input .GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveY = Input .GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(new Vector3(moveX, moveY, 0));
+        
+
+        if (mainCamera != null)
+        {
+            mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 5f);
+        }
+    }
+}
